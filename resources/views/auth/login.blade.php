cbf6525b6be96cbd973a20017c9bdf2df2913182
dwiliandy <dwiliandy@gmail.com>
2023-01-02 16:17:58 +0700

    Sign-in Form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,62 +1,88 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo width="82" />
-            </a>
-        </x-slot>
+@extends('layouts.auth.app',[
+  'title' =>'Sign In'
+])
 
-        <div class="card-body">
-            <!-- Session Status -->
-            <x-auth-session-status class="mb-3" :status="session('status')" />
+@push('css')
+<style>
+  .bd-placeholder-img {
+    font-size: 1.125rem;
+    text-anchor: middle;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    user-select: none;
+  }
 
-            <!-- Validation Errors -->
-            <x-auth-validation-errors class="mb-3" :errors="$errors" />
+  @media (min-width: 768px) {
+    .bd-placeholder-img-lg {
+      font-size: 3.5rem;
+    }
+  }
 
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
+  .b-example-divider {
+    height: 3rem;
+    background-color: rgba(0, 0, 0, .1);
+    border: solid rgba(0, 0, 0, .15);
+    border-width: 1px 0;
+    box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+  }
 
-                <!-- Email Address -->
-                <div class="mb-3">
-                    <x-label for="login" :value="__('Email')" />
+  .b-example-vr {
+    flex-shrink: 0;
+    width: 1.5rem;
+    height: 100vh;
+  }
 
-                    <x-input id="login" type="text" name="login" :value="old('email')" required autofocus />
-                </div>
+  .bi {
+    vertical-align: -.125em;
+    fill: currentColor;
+  }
 
-                <!-- Password -->
-                <div class="mb-3">
-                    <x-label for="password" :value="__('Password')" />
+  .nav-scroller {
+    position: relative;
+    z-index: 2;
+    height: 2.75rem;
+    overflow-y: hidden;
+  }
 
-                    <x-input id="password" type="password"
-                             name="password"
-                             required autocomplete="current-password" />
-                </div>
+  .nav-scroller .nav {
+    display: flex;
+    flex-wrap: nowrap;
+    padding-bottom: 1rem;
+    margin-top: -1px;
+    overflow-x: auto;
+    text-align: center;
+    white-space: nowrap;
+    -webkit-overflow-scrolling: touch;
+  }
+</style>
+@endpush
 
-                <!-- Remember Me -->
-                <div class="mb-3">
-                    <div class="form-check">
-                        <x-checkbox id="remember_me" name="remember" />
 
-                        <label class="form-check-label" for="remember_me">
-                            {{ __('Remember Me') }}
-                        </label>
-                    </div>
-                </div>
+@section('content')
+  <main class="form-signin w-100 m-auto">
+    <form method="POST" action="{{ route('login') }}">
+    @csrf
+       {{-- <img class="mb-4 img-fluid  mx-auto d-block" src="{{ asset('sion-1.jfif') }}" alt="" width="200" height="200"> --}}
+    @if ($errors->any())
+      <div class="alert alert-danger border-left-danger" role="alert">
+          {{ $errors->first() }}
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+    @endif
+      {{-- <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> --}}
+      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
-                <div class="mb-0">
-                    <div class="d-flex justify-content-end align-items-baseline">
-                        @if (Route::has('password.request'))
-                            <a class="text-muted me-3" href="{{ route('password.request') }}">
-                                {{ __('Forgot your password?') }}
-                            </a>
-                        @endif
+      <div class="form-floating">
+        <input type="text" name="login" class="form-control" id="floatingInput" placeholder="Email/Username">
+        <label for="floatingInput">Email/Username</label>
+      </div>
+      <div class="form-floating">
+        <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
+      </div>
 
-                        <x-button>
-                            {{ __('Log in') }}
-                        </x-button>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </x-auth-card>
-</x-guest-layout>
+      <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">&copy; Omega</p>
+    </form>
+  </main>
+@endsection
