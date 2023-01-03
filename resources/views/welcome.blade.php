2dadde959ee928e8d6acdfca50dadf571a77e2bc
dwiliandy <dwiliandy@gmail.com>
2023-01-03 17:47:38 +0700

    add contact us

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 ])
 
 @section('content')
+{{-- Slider --}}
 <div id="slider" class="carousel slide">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#slider" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -28,6 +29,26 @@
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
+</div>
+{{-- End Slider --}}
+
+<div id="contact_us" style="min-height: 100vh; background-color: rgba(255, 255, 255, 0.2)">
+  <h1 class="text-center p-3 bold">HUBUNGI KAMI</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 justify-content-center align-self-center">
+        <h2 >GMIM Rut Sendangan</h2>
+        Jl. Arjuna Utara No.35, RT.8/RW.1, <br>
+        Duri Kepa, Kec. Kb. Jeruk, Kota Jakarta Barat <br>
+        DKI Jakarta 11530, Indonesia
+        <br>
+        +62 813 2900 1097
+      </div>
+      <div class="col-md-6 justify-content-center align-self-center">
+        <div class="map" style="height:400px;width:100%;background-color:blue"></div>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
 
