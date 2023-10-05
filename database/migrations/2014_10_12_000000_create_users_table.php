7c320bcab902834fe09c7d46c281fde0adb3dafc
dwiliandy <dwiliandy@gmail.com>
2023-10-05 14:35:31 +0700

    tambah web setting

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,8 @@ return new class extends Migration
             $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('approval_income')->nullable();
+            $table->boolean('approval_expenditure')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
